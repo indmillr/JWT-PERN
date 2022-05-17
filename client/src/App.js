36457796af895ab017cace50ef5ea277a4b0434b
Ian Miller <theiandmiller@gmail.com>
2022-05-17 06:52:55 -0500

    updates'

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,42 @@ import React, { Fragment } from "react";
 import "./App.css";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Redirect,
+  // Redirect,
 } from "react-router-dom";
 
+// Components
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 function App() {
-  return <Fragment />;
+  return (
+    <Fragment>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/login'
+              render={(props) => <Login {...props} />}
+            />
+            <Route
+              exact
+              path='/register'
+              render={(props) => <Register {...props} />}
+            />
+            <Route
+              exact
+              path='/dashboard'
+              render={(props) => <Dashboard {...props} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
