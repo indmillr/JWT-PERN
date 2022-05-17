0ba3eb44a13e1b5c5a2b6554d9d28143556b405e
Ian Miller <theiandmiller@gmail.com>
2022-05-17 05:44:16 -0500

    token verification to change state

--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-  const { email, name, passowrd } = req.body;
+  const { email, name, password } = req.body;
 
   function validEmail(userEmail) {
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+/$.test(userEmail);
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
   }
 
   if (req.path === "/register") {
@@ -12,15 +12,13 @@ module.exports = (req, res, next) => {
     } else if (!validEmail(email)) {
       return res.status(401).json("Invalid Email");
     }
-  }
-  
-  else if (req.path === "/login") {
+  } else if (req.path === "/login") {
     if (![email, password].every(Boolean)) {
       return res.status(401).json("Missing Credentials");
     } else if (!validEmail(email)) {
       return res.status(401).json("Invalid Email");
     }
-    }
+  }
 
-    next();
-  };
+  next();
+};
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,6 +3,7 @@ const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 const validInfo = require("../middleware/validInfo");
+const authorization = require("../middleware/authorization");
 
 // ---------- REGISTER ----------
 
@@ -74,6 +75,15 @@ router.post("/login", validInfo, async (req, res) => {
     // Step 4: Give the JWT Token
     const token = jwtGenerator(user.rows[0].user_id);
     res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/is-verify", authorization, async (req, res) => {
+  try {
+    res.json(true);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
